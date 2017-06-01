98ec644cef672ded92c49ae6910ed928b6bcda85
jorgesate <jorgesate@gmail.com>
2017-06-01 19:28:53 +0200

    KNN un poco mas

--- KNNClassifier.py
+++ KNNClassifier.py
@@ -5,16 +5,22 @@ from sklearn.model_selection import cross_val_score
 import numpy as np
 import math
 
+# KNeighborsClassifier(n_neighbors=5, weights='uniform''distance', algorithm='auto',
+#       leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=1, **kwargs)
 
 class KNNClassifier(BaseEstimator, ClassifierMixin):
 
     def __init__(self):
         self.neigbours = 0
+        self.kmax = 5
         self.KNNclsf = None
 
-    def fit(self):
+    def fit(self, X, y):
 
-        self.KNNclsf = KNeighborsClassifier(n_neighbors=self.neigbours, weights=self.weights)
+        for k in range(1, self.kmax):
+            self.KNNclsf = KNeighborsClassifier(n_neighbors=self.neigbours)
+            # TODO probar con weights 'distance' tambien
+            scores = cross_val_score(self.KNNclsf, X, y, cv=5, scoring='f1_macro')
 
     def predict(self):
 
